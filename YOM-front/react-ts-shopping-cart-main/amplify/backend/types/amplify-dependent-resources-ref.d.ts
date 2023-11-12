f4b58ade1a06f631f8b0448f8fdc350ec9fcc61e
Ch1jik <nikolo174@gmail.com>
2023-11-12 21:10:31 +0100

    Update amplify

---
+++ YOM-front/react-ts-shopping-cart-main/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "yom": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
